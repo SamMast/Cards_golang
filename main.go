8fba081cbc87c4e555f6efed583b9ade4c110a21
Samuel Mast <samuel.a.mast@gmail.com>
2018-05-23 15:09:51 -0600

    variables

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// These 2 lines of code are the same:
+	// var card string = "Ace of Spades"
+	// only use := when assigning a new variable
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
