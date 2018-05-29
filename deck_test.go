050ddbbfe0e8d126c2b103bec7eeac77f859e398
Samuel Mast <samuel.a.mast@gmail.com>
2018-05-29 17:26:26 -0400

    update to testing

--- deck_test.go
+++ deck_test.go
@@ -13,7 +13,7 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
 	}
 
-	if d[51] != "King of Clubs" {
-		t.Errorf("Expected last card to be King of Clubs, but got %v", d[51])
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but got %v", d[len(d)-1])
 	}
 }
